01ea0f3c772bbbcbe3344040d96499abca752bf4
crashtack <crashtack@gmail.com>
2016-06-20 14:26:30 -0700

    Stretch Goal: moving the admin page to the index page

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -3,6 +3,7 @@
 
   page('/', articleController.index);
   page('/about', aboutController.index);
+  page('/stats', statsController.index);
   page();
 /* TODO: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
