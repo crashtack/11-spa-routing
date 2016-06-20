8da017d458f1700666307317807031ba2783d8fa
crashtack <crashtack@gmail.com>
2016-06-20 13:32:13 -0700

    completed articleController.js function TODO

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -2,7 +2,10 @@
   var articleController = {};
 
   articleController.index = function() {
+    Article.fetchAll(articleView.renderIndexPage);
     /* TODO: Reveal only the articles section! */
+    $('.tab-content').hide();
+    $('#articles').fadeIn();
   };
 
   module.articleController = articleController;
