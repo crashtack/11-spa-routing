21bf0ec9daf877cf7ad35339f73077e189bc5bab
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-20 14:07:38 -0700

    fixed order of page() calls

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,9 +1,9 @@
 /* TODO: What function do we call to activate page.js?
     Fire it off now, to execute it: */
-  page();
+
   page('/', articleController.index);
   page('/about', aboutController.index);
-
+  page();
 /* TODO: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
