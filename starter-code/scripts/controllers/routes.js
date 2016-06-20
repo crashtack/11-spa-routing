569e8f07260e1c00f7155f9dedc3fd6f1290df6e
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-20 13:43:06 -0700

    completed routes.js to activate page.js

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,6 +1,8 @@
 /* TODO: What function do we call to activate page.js?
     Fire it off now, to execute it: */
   page();
+  page('/', articleController.index);
+  page('/about', aboutController.index);
 
 /* TODO: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
